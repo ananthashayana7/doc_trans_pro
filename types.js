51f747324dfabfa804e7013a053c9facc1e7b376
Anantha Shayana <anantha.shayana10@gmail.com>
2025-12-29 15:01:47 +0530

    Delete types.js

--- types.js
+++
@@ -1,9 +0,0 @@
-export const SUPPORTED_LANGUAGES = [
-  { code: 'en', name: 'English' },
-  { code: 'es', name: 'Spanish' },
-  { code: 'fr', name: 'French' },
-  { code: 'de', name: 'German' },
-  { code: 'it', name: 'Italian' },
-  { code: 'ja', name: 'Japanese' },
-  { code: 'hi', name: 'Hindi' }
-];
